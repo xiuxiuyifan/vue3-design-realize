61b2401743ae81413e84f55a139fe7f7f411d739
xiuxiuyifan <1575243732@qq.com>
2022-11-13 18:09:32 +0800

    feat: 9.2实现effect函数的调度执行

--- src/9.调度执行.js
+++ src/9.调度执行.js
@@ -5,7 +5,7 @@ let activeEffect
 
 let effectStack = [] // 存放 effect 函数的执行栈
 
-function effect(fn) {
+function effect(fn, options = {}) {
   const effectFn = () => {
     // 在触发 get 之前先清除
     cleanup(effectFn)
@@ -19,6 +19,9 @@ function effect(fn) {
     // 取栈顶的 effectFn赋值给 activeEffect
     activeEffect = effectStack[effectStack.length - 1]
   }
+
+  // 将 options 挂载到 effectFn 身上
+  effectFn.options = options
   // 用来存贮包含所有副作用的集合。
   effectFn.deps = []
   // 执行副作用函数6
@@ -54,6 +57,8 @@ const obj = new Proxy(data, {
 // }
 
 function track(target, key) {
+  // 如果没有 activeEffect 则直接 return 可能是直接访问的
+  if (!activeEffect) return
   // 查找对象是否在依赖中出现过
   let targetMap = weakmap.get(target)
   // 如果不存在 targetMap 则新建一个 map 赋值给 targetMap 并与 target 关联 ，并且把 其存放到 weakmap中去
@@ -93,7 +98,13 @@ function trigger(target, key) {
     }
   })
   effectToRun.forEach(effectFn => {
-    effectFn()
+    //如果一个副作用函数存在调度器参数，那么则调用这个调度器函数，并将副作用函数作为参数进行传递
+    if (effectFn.options.scheduler) {
+      effectFn.options.scheduler(effectFn)
+    } else {
+      // 否则还是执行副作用函数
+      effectFn()
+    }
   })
 }
 
@@ -107,17 +118,66 @@ function cleanup(effectFn) {
   effectFn.deps.length = 0
 }
 
+// 通过调度器控制effect函数的执行次数
 
+const jobQueue = new Set()
+
+const p = Promise.resolve()
+// 表示是否正在刷新队列
+let isFlushing = false
+
+function flushJob() {
+  // 如果当前队列正在刷新，则什么都不做
+  if (isFlushing) return
+  isFlushing = true
+  // 在微任务队列中刷新任务
+  p.then(() => {
+    jobQueue.forEach((job) => {
+      job()
+    })
+  })
+    .finally(() => {
+      isFlushing = false
+    })
+}
+
+
+
+// effect(function effect1() {
+//   console.log(obj.count)
+// }, {
+//   // 调度器 scheduler 是一个函数， 被调用的时候会传入当前的 effect 函数，让用户可以在，再次触发 trigger函数的时候手动调用
+//   scheduler: (fn) => {
+//     // 使用一个异步函数，
+//     setTimeout(() => {
+//       fn()
+//     }, 0);
+//   }
+// })
 
 effect(function effect1() {
-  obj.count++
+  console.log(obj.count)
+}, {
+  // 调度器 scheduler 是一个函数， 被调用的时候会传入当前的 effect 函数，让用户可以在，再次触发 trigger函数的时候手动调用
+  scheduler: (fn) => {
+    // 当触发依赖的时候将副作用函数加入到  jsbQueue 中
+    jobQueue.add(fn)
+    // 同时调用刷新任务的函数， 当同步触发多次 scheduler 函数的时候，会在 flushJob 函数中消除掉多余的调用次数
+    flushJob()
+  }
 })
 
+obj.count++
+obj.count++
 
-console.log(weakmap)
+console.log('结束了')
+
 
 
 
 // 解决的问题，
 
 // effect 添加一个参数来实现调度执行，把 effect 执行的控制权，可以交给开发者。
+
+// 一定要记住是设置响应式数据的时候才触发依赖
+// 在effect 函数里面触发的 get 才会执行   obj.count 虽然也触发了 get 但是此时的 activeEffect为 undefined

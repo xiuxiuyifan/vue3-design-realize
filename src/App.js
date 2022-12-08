7cb5f835bbebbf4515116b74267aa28f7191a1da
xiuxiuyifan <1575243732@qq.com>
2022-12-08 22:13:55 +0800

    13.1 异步组件要解决的问题 自己用 import() 实现一个异步组件

---
+++ src/App.js
@@ -0,0 +1,8 @@
+
+
+const App = {
+  type: 'div',
+  children: '我是App组件'
+}
+
+export default App
---
+++ src/异步组件与函数式组件.js
@@ -0,0 +1,23 @@
+
+// import App from './App.js'
+import { Fragment, onMounted, renderer } from "./renderer"
+
+
+
+// renderer.render(App, document.getElementById('root'))  // 同步事件组建的渲染
+
+
+
+// 演示加载
+
+setTimeout(() => {
+  // 下面展示异步实现
+
+  const loader = () => import('./App.js')
+
+  // 异步加载组件
+  loader()
+    .then((App) => {
+      renderer.render(App.default, document.getElementById('root'))
+    })
+}, 1000);
